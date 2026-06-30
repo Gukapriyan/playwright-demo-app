1a0836f8a3883a3cbc0e41c61ebccff0fd99290b
Gukan <88232274+Gukapriyan@users.noreply.github.com>
2026-06-30 18:00:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
   console.log(`App running at http://localhost:${port}`);
   console.log(`App running at http://localhost:${port}`);
+  console.log(`App running at http://localhost:${port}`);
 
   console.log(`App running at http://localhost:${port}`);
   console.log(`App running at http://localhost:${port}`);
